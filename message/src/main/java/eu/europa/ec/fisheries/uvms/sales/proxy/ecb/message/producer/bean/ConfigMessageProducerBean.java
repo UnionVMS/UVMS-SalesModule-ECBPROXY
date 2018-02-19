246ba48119fb45f699ebe71c09cb543aaadb07f1
KRISCH <kris.schockaert@lv.vlaanderen.be>
2018-02-19 14:11:51 +0100

    Product Backlog Item #142165 Integratie Communication Library Update dependency commons-message 3.0.9. Task #150538 Aanpassen modules voor  commons message nieuwe release versie Task #152377 commons-message 3.0.9-SNAPSHOT AbstractProducer sendModuleResponse garandeerd zenden van module response en fault berichten

--- message/src/main/java/eu/europa/ec/fisheries/uvms/sales/proxy/ecb/message/producer/bean/ConfigMessageProducerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/sales/proxy/ecb/message/producer/bean/ConfigMessageProducerBean.java
@@ -1,11 +1,11 @@
 package eu.europa.ec.fisheries.uvms.sales.proxy.ecb.message.producer.bean;
 
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
+import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractProducer;
 import eu.europa.ec.fisheries.uvms.commons.message.impl.JMSUtils;
 import eu.europa.ec.fisheries.uvms.config.exception.ConfigMessageException;
 import eu.europa.ec.fisheries.uvms.config.message.ConfigMessageProducer;
 import eu.europa.ec.fisheries.uvms.sales.proxy.ecb.message.constants.SalesEcbProxyMessageConstants;
-import eu.europa.ec.fisheries.uvms.sales.proxy.ecb.message.producer.ExtendedAbstractProducer;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.ejb.Stateless;
@@ -15,7 +15,7 @@ import javax.jms.Queue;
 
 @Slf4j
 @Stateless
-public class ConfigMessageProducerBean extends ExtendedAbstractProducer implements ConfigMessageProducer {
+public class ConfigMessageProducerBean extends AbstractProducer implements ConfigMessageProducer {
 
     private static final long TIME_TO_LIVE = 60000L;
 
@@ -24,7 +24,7 @@ public class ConfigMessageProducerBean extends ExtendedAbstractProducer implemen
     public String sendConfigMessage(String configMessage) throws ConfigMessageException {
         try {
             Queue replyToQueue = JMSUtils.lookupQueue(SalesEcbProxyMessageConstants.QUEUE_ECB_PROXY_CONFIG);
-            return sendModuleMessageNonePersistent(configMessage, replyToQueue, TIME_TO_LIVE);
+            return sendModuleMessageNonPersistent(configMessage, replyToQueue, TIME_TO_LIVE);
 
         } catch (Exception e) {
             String errorMessage = "Unable to send config messages for sales ecb proxy. Reason: " + e.getMessage();
