8ed51b2ae2710e6253a2d77633c7077fca9d53df
MathiasBulte <bulte.mathias@gmail.com>
2018-10-02 08:30:55 +0200

    fix powermock issues

--- domain/src/test/java/eu/europa/ec/fisheries/uvms/sales/proxy/ecb/domain/bean/ExchangeRateDomainModelBeanTest.java
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/sales/proxy/ecb/domain/bean/ExchangeRateDomainModelBeanTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 
--- service/src/test/java/eu/europa/ec/fisheries/uvms/sales/proxy/ecb/service/bean/EcbProxyClientBeanTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/sales/proxy/ecb/service/bean/EcbProxyClientBeanTest.java
@@ -12,7 +12,7 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 
